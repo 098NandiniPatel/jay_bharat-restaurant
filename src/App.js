f436f448c0e5f5c0146f53c574876daf9cebbd35
098NandiniPatel <patelnndn@gmail.com>
2023-07-17 17:23:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Pages/Footer";
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import Gallery from "./Pages/Gallery";
+import ContactUs from "./Pages/ContactUs";
+import ScrollToTop from 'react-scroll-to-top';
+import {BsChevronDoubleUp} from 'react-icons/bs'
+import NavBar from './components/header/Navbar';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+      
+        <Route path="/" element={<Home />}  />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
+     
+      <Footer />
+      <ScrollToTop smooth top={300} component={<BsChevronDoubleUp/>} style={{backgroundColor:'#db4744' ,color:'#fff', fontSize:'18px'}} />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{paddingTop:'100px'}}>
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam accusamus, aut temporibus, consectetur earum quibusdam laboriosam error nemo aspernatur impedit, provident perspiciatis voluptatum minima sapiente natus! Non fugiat dolorem vitae!
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos fugit facere quasi recusandae sapiente ullam deleniti nostrum inventore sint in molestiae dolores aut modi, omnis assumenda maxime voluptatum eius rem.
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ea ducimus velit maxime numquam hic illum eum magnam eius quibusdam quae animi, sit voluptates quam quos fuga ab dicta nulla.
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, autem corrupti. Officia debitis aspernatur repudiandae enim? Quisquam ipsum, distinctio esse dolores, voluptatibus atque veniam dolorum quis odio saepe quibusdam blanditiis!
+      <br/>
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos fugit facere quasi recusandae sapiente ullam deleniti nostrum inventore sint in molestiae dolores aut modi, omnis assumenda maxime voluptatum eius rem.
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ea ducimus velit maxime numquam hic illum eum magnam eius quibusdam quae animi, sit voluptates quam quos fuga ab dicta nulla.
+      <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, autem corrupti. Officia debitis aspernatur repudiandae enim? Quisquam ipsum, distinctio esse dolores, voluptatibus atque veniam dolorum quis odio saepe quibusdam blanditiis!
+      <br/>
+    </div>
+  )
+}
+
+export default Home
